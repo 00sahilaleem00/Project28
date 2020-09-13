5420b1b782379296e67098de5088547f741a49ea
Sahil Aleem <69380600+00sahilaleem00@users.noreply.github.com>
2020-09-13 15:01:07 +0530

    Update tree.js

--- tree.js
+++ tree.js
@@ -1,17 +1,13 @@
 class Tree{
     constructor(){
-        var options = {
-            isStatic:false
-        }
-
-        this.body = Bodies.rectangle(600,350,350,350,options);
+        this.x = 600;
+        this.y = 430;
         this.width = 350;
         this.height = 350;
         this.image = loadImage('tree.png');
-        World.add(world,this.body);
     }
     display(){
         imageMode(CENTER);
-        image(this.image,590,430,this.width,this.height);
+        image(this.image,this.x,this.y,this.width,this.height);
     }
 }
