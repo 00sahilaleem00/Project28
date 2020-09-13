b403247d72f246a5fc239df209aed79c28df5d43
Sahil Aleem <69380600+00sahilaleem00@users.noreply.github.com>
2020-09-13 15:00:49 +0530

    Update stone.js

--- stone.js
+++ stone.js
@@ -1,20 +1,19 @@
 class Stone {
     constructor(){
-        var options= {
+        var options={
             isStatic:false,
             restitution:0,
-            friction:1,
-            density:1.2
+            friction:1
         }
 
-        this.body = Bodies.rectangle(300,100,50,50,options);
-        this.width = 50;
-        this.height = 50;
+        this.body = Bodies.rectangle(250,390,20,20,options);
+        this.width = 20;
+        this.height = 20;
         this.image = loadImage('stone.png');
         World.add(world, this.body);
     }
       display(){
         imageMode(CENTER);
-        image(this.image, 300,100, this.width, this.height);
+        image(this.image,this.body.position.x, this.body.position.y, this.width*2.2, this.height*2.2);
     }
 }
