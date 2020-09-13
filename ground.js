4fc96ccd83d1ab2a38f405e45a485f80415a5d89
Sahil Aleem <69380600+00sahilaleem00@users.noreply.github.com>
2020-09-13 14:58:13 +0530

    Update ground.js

--- ground.js
+++ ground.js
@@ -1,12 +1,15 @@
 class Ground{
     constructor(){
-        this.body = Bodies.rectangle(width/2,height-100,width,20);
+        var options = {
+            isStatic:true
+        }
+        this.body = Bodies.rectangle(width/2,height-100,width,20,options);
         this.width = width;
         this.height = 20;
         World.add(world,this.body);
     }
     display(){
-        rectMode(CENTER)
-        rect(width/2,height-100,this.width,this.height)
+        rectMode(CENTER);
+        rect(this.body.position.x,this.body.position.y,this.width,this.height);
     }
 }
